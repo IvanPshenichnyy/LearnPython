4728ade13d2b3c0c236b8941a12cb745aadcdba0
IvanPshenichnyy <iv.psheni4nii@yandex.ru>
2024-05-12 02:34:22 +0300

    Откатился назад

--- main.py
+++
@@ -1,8 +0,0 @@
-# name = input("Enter your name: ")
-# age = input("Enter your age: ")
-# print(f"your name {name.capitalize()} your age {age}")
-name = 'ivan'
-# print(dir(name))
-print(name.count('iva'))
-print(name.casefold())
- 
